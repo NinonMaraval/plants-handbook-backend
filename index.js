8e1975aa90e210c25f127eeacae45cbd01e631b4
Ninon Maraval <maravalninon@gmail.com>
2021-07-26 18:49:17 +0200

    cors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable consistent-return */
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -10,8 +9,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
+// eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -19,7 +23,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested, Content-Type, Accept Authorization'
   );
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE');
+    res.header('Access-Control-Allow-Methods', 'POST, PATCH, GET');
     return res.status(200).json({});
   }
   next();
