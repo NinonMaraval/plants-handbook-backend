37a87cd32eb7b583316003656d4fb984f05b8368
Ninon Maraval <maravalninon@gmail.com>
2021-07-27 14:18:47 +0200

    app

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-// TODO: configure cors
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  })
-);
+app.use(cors());
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested, Content-Type, Accept Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 module.exports = app;
