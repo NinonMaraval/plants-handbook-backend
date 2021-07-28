fb018dd39417974dba5aca8abcafcf8fba03e090
Ninon Maraval <maravalninon@gmail.com>
2021-07-28 09:29:58 +0200

    test

--- indexOld.js
+++ indexOld.js
@@ -1,17 +1,26 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const plantRouter = require('./routes/plant');
+const categoryRouter = require('./routes/category');
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/plant', plantRouter);
+app.use('/category', categoryRouter);
 
 app.listen(PORT, (err) => {
   if (err) {
