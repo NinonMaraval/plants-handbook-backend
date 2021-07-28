748f702d722f1b41295f8cd673b8603b56b12dff
Ninon Maraval <maravalninon@gmail.com>
2021-07-28 09:38:20 +0200

    added netlify url

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const categoryRouter = require('./routes/category');
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 
